d177ac18b97b5e858e2e460e4a4a2665f456031e
Shivam Mishra <scmmishra@users.noreply.github.com>
2019-07-26 17:18:38 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,10 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
-from pip.req import parse_requirements
 
 version = '0.0.1'
-requirements = parse_requirements("requirements.txt", session="")
+
+with open('requirements.txt') as f:
+	requirements = f.read().strip().split('\n')
 
 setup(
 	name='vidya',
@@ -14,6 +15,5 @@ setup(
 	packages=find_packages(),
 	zip_safe=False,
 	include_package_data=True,
-	install_requires=[str(ir.req) for ir in requirements],
-	dependency_links=[str(ir._link) for ir in requirements if ir._link]
+	install_requires=requirements
 )
